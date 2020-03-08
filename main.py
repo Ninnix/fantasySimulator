f838c9f955ea9445e8cc83809a9db855c81bb0ae
Ninnix <nicolo.deva@gmail.com>
2020-03-08 17:35:08 +0100

    Update main.py
    ora puoi creare il tuo team e controllare se è valido
    
    alcuni attributi di drivers ora sono liste (molte cose da rivere li)

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import csv
 
 budget = 100
 
+id_d0 = '1'
+id_d1 = '815'
+id_d2 = '154'
+id_d3 = '825'
+id_d4 = '847'
+id_t0 = '131'
+
 class Driver:
-    def __init__(self, name , id, team, team_id, mate, mate_id, price, points):
+    def __init__(self, name , id, team, team_id, mate, mate_id, price, turbo, points):
         self.name = name
         self.id = id
         self.team = team
@@ -11,6 +18,7 @@ class Driver:
         self.mate = mate
         self.mate_id = mate_id
         self.price = price
+        self.turbo = turbo
         self.points = points
 
 class Team:
@@ -49,9 +57,7 @@ with open('fantasy_driver.csv') as d:
     lst_drivers = []
     for row in reader_f_driver:
         if row[0] != 'Driver':
-            lst_drivers.append(Driver(row[0], row[1], row[2], row[3], row[4], row[5], row[6], 0))
-    for d in lst_drivers:
-        print(d.name)
+            lst_drivers.append(Driver(row[0], row[1], row[2], row[3], row[4], row[5], row[6], False, 0))
 
 with open('fantasy_team.csv') as t:
     reader_f_team = csv.reader(t)
@@ -59,16 +65,34 @@ with open('fantasy_team.csv') as t:
     for row in reader_f_team:
         if row[0] != 'Team':
             lst_teams.append(Team(row[0], row[1], row[2], 0))
-    for t in lst_teams:
-        print(t.name)
-
+    
 def validate(d0, d1, d2, d3, d4, t0):
-    tot = d0.price + d1.price + d2.price + d3.price + d4.price + t0.price
+    tot = float(d0.price) + float(d1.price) + float(d2.price) + float(d3.price) + float(d4.price) + float(t0.price)
     if (tot > budget):
         print('invalid team')
         return False
+    print('team is ok')
     return True
+
 
 def simulation(d0, d1, d2, d3, d4, t0):
     if validate(d0, d1, d2, d3, d4, t0) == False: 
         return
+
+
+for d in lst_drivers:
+    if d.id == id_d0: 
+        my_driver0 = d
+    if d.id == id_d1: 
+        my_driver1 = d
+    if d.id == id_d2: 
+        my_driver2 = d
+    if d.id == id_d3: 
+        my_driver3 = d
+    if d.id == id_d4: 
+        my_driver4 = d
+for t in lst_teams:
+    if t.id == id_t0:
+        my_team = t
+        
+validate(my_driver0, my_driver1, my_driver2, my_driver3, my_driver4, my_team)
